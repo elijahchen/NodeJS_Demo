9cf653cc3683f405df852b75d8bbd19577342214
elijahchen <code.elijahchen@gmail.com>
2017-03-04 20:11:50 -0500

    - Testing npm packages

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 /**
  * Created by Elijah on 3/4/2017.
  */
-console.log("Hello");
+var cats = require("cat-me");
+
+console.log(cats());
+
+var knocks = require("knock-knock-jokes");
+
+console.log(knocks());
