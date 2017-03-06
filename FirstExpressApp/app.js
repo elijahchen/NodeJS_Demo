9b42182080b6d56b4d27be9439daa17848952443
elijahchen <code.elijahchen@gmail.com>
2017-03-05 19:50:33 -0500

    - Testing of route matching in express

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+//ORDER OF ROUTES IS VERY IMPORTANT
+//IF ANY CALLBACK HAS BEEN TRIGGERED, IT IS CONSIDERED HANDLED!!
+
 // "/" => "Hi there!", { / } indicates root directory
 app.get("/", function(request, response) {
     response.send("Hi there!");
@@ -8,12 +11,29 @@ app.get("/", function(request, response) {
 
 app.get("/bye", function (request, response) {
     response.send("Bye!");
-})
+});
 
 app.get("/dog", function (request, response) {
     console.log("Someone made a request to /dog");
     response.send("Woof!")
-})
+});
+
+//Pattern matching, will try to match a route after /r/..
+app.get("/r/:subredditName", function (request, response) {
+    // console.log(request); // params: { subredditName: 'home' }
+    var subreddit = request.params.subredditName;
+    response.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+//Further limitations of pattern matching, following /
+app.get("/r/:subredditName/comments/:id/:title/", function (request, response) {
+    response.send("WELCOME TO THE COMMENTS PAGE!")
+});
+
+//ERROR or route that is not expected, "PAGE NOT FOUND"
+app.get("*", function (request, response) {
+    response.send("ASTERISK!");
+});
 
 //process.env.PORT, process.env.IP
 app.listen(3000, process.env.IP, function () {
