0e95993f89af85b9ddd299b4980f5e3e3bd0d79f
elijahchen <code.elijahchen@gmail.com>
2017-03-06 21:31:32 -0500

    - Cont. practice with express & EJS package

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -5,6 +5,16 @@ app.get("/", function (req, res) {
     res.render("home.ejs");
 });
 
+app.get("/posts", function (req, res) {
+   var posts = [
+       {title: "FIRST POST!!", author: "Suzy"},
+       {title: "Donald Trump reveals it all!", author: "OfficialDT"},
+       {title: "Dalai Lama will not reincarnate", author: "REAL_DL"},
+   ];
+
+   res.render("posts.ejs", {posts: posts});
+});
+
 app.get("/fallinlovewith/:thing", function (req, res) {
     var thing = req.params.thing;
     res.render("love.ejs", {thingVar: thing});
